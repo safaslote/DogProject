b58b281033967ec180090a6d0882747c8a9bf94b
safaslote <safaslote@users.noreply.github.com>
2019-02-23 11:11:59 -0600

    Changed dog color to red

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
 	
 	public static void main(String[] args) {
-		Dog rufus = new Dog("Brown", 48, "Doberman Pincher", "Rufus");
+		Dog rufus = new Dog("Red", 48, "Doberman Pincher", "Rufus");
 
 		rufus.getColor();
 
